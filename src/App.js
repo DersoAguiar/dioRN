42a822f6a94c3a0f663dcca3ddadfbfd528a100a
DersoAguiar <andersonaguiar.m.z@gmail.com>
2022-05-27 18:32:54 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,121 @@
+import React from "react";
+import {View, Image, Text, StyleSheet, SafeAreaView, StatusBar, Pressable, Linking} from 'react-native';
+
+const colorGithub = '#010409';
+const colorFontGitHub = '#c9d1d9'
+const colorDarkFontGitHub = '#8b949e'
+
+const imageProfile = 'https://avatars.githubusercontent.com/u/87333149?v=4';
+
+const urlToMyGitHub = 'https://github.com/DersoAguiar';
+
+const App = () => {
+
+    const handlePressGoToGitHub = async () => {
+        console.log('Verificando Link');
+        const res = await Linking.canOpenURL(urlToMyGitHub);
+        if(res) {
+            console.log('Link Aprovado');
+            console.log('Abrindo Link...');
+            await Linking.openURL(urlToMyGitHub);
+        }
+    }
+    return (
+        // safeareaview é para celulares com o notche
+        <SafeAreaView style={style.container}>
+            <StatusBar backgroundColor={colorGithub} barStyle="light-content" />
+        <View style={style.content}>
+            <Image 
+            accessibilityLabel="foto de Anderson" 
+            style={style.avatar} 
+            source={{uri: imageProfile}}
+            />
+            <Text 
+                accessibilityLabel= "Nome: Anderson S Aguiar" 
+                style={[style.defautlText, style.name]}>
+                Anderson S Aguiar
+                </Text>
+            <Text 
+                accessibilityLabel= "Nickname: DersoAguiar"
+                style={[style.defautlText, style.nickname]}>
+                    DersoAguiar
+            </Text>
+            <Text 
+                accessibilityLabel="Descrição: Aprendendo Front-End | JavaScript | HTML5 | CCS3 | React | React Native |
+                Insta: @s_a.anderson"
+                style={[style.defautlText, style.description]}>
+                Aprendendo Front-End | JavaScript | HTML5 | CCS3 | React | React Native |
+                Insta: @s_a.anderson
+            </Text>
+
+            <Pressable onPress={handlePressGoToGitHub}>
+
+            <View style={style.button}> 
+                <Text style={[style.defautlText, style.textButton]}> Open in GitHub </Text>
+            </View>
+            
+            </Pressable>
+        </View> 
+
+        </SafeAreaView>
+    );
+    
+;}
+
+export default App;
+
+const style = StyleSheet.create({
+    container: { // colums
+        backgroundColor: colorGithub,
+        flex: 1, // Expandir para a tela inteira
+        justifyContent: 'center',
+        alignItems: 'center',
+        // flexDirection: 'row',
+
+    },
+
+    content:{
+        alignItems: 'center',
+        padding: 20,
+    },
+
+    avatar:{
+        height: 250,
+        width: 250,
+        borderRadius: 125,
+        borderColor: 'white',
+        borderWidth: 1.5,
+    },
+
+    defautlText: {
+        color: colorFontGitHub,
+
+    },
+    
+    name: {
+        fontWeight: 'bold',
+        fontSize: 24,
+        marginTop: 20,
+          
+    },
+    nickname: {
+        fontSize: 18,
+        color: colorDarkFontGitHub,
+    },
+    description: {
+        fontWeight: 'bold',
+        fontSize: 14,
+        
+    },
+    button: {
+        marginTop: 20,
+        backgroundColor: colorDarkFontGitHub,
+        borderRadius: 10,
+        padding: 20,
+    },  
+    textButton: {
+        fontWeight: 'bold',
+        fontSize: 16,
+        
+    },
+});
